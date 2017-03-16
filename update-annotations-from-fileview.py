6bc290782004a55194eda4ddcb74978b375c9452
teslajoy <nasim@plenary.org>
2017-03-16 15:53:41 -0700

    added functions and updated final goal

--- update-annotations-from-fileview.py
+++ update-annotations-from-fileview.py
@@ -1,78 +1,63 @@
-# parse through a file View and annotate them based on user-defined schema association (file name | synapse ID , annotation)
-# example syn8450784
+# Get a generator file view make changes to data frame push up changes to file view 
 # !!! Only works if file view (and its associated schema) exist !!! 
 
 import synapseclient
 import synapseutils 
 import pandas 
-import requests 
 
-filename = 'userDefined_annotations.csv'
-user_metaData_annotation = pandas.read_csv(filename, index_col='synapseId')
-t_data = user_metaData_annotation.transpose() # column access is faster 
-t_data = t_data.to_dict()
+path = 'userDefined_annotations.csv'
 
-# print(t_data.get('syn8450577'))
-# print(t_data.keys()) # now all the synIDs are keys
-
-# connect to synapse
-
-syn = synapseclient.Synapse()
-syn.login()
-
-# list of existing file views and associated schema to be annotated 
+# list of existing file views and associated existing schema to be annotated 
 fileViewIds = ["syn8455906"] 
 fileView = []
+query = "select * from "
+tag = "id"
 
+def getCSV(path):
+	# use'synapseId'as col index for now 
+	# TODO: pass args to panda read csv
+	df = pandas.read_csv(path, index_col='synapseId')
+	return(df)
+
+def fastAccess(df):
+	# column or key access is faster 
+	df = df.transpose() 
+	dc = df.to_dict()
+	return df, dc
+
+def connectToSynClient():
+	syn = synapseclient.Synapse()
+	syn.login()
+	return(syn)
+
+def downloadQueryAndmakeDf(syn, query, index, fileViewIds):
+	view = syn.tableQuery(query + fileViewIds[index])
+	df = view.asDataFrame()
+	return(df)
+
+def uniqueIds(df, tag):
+	unique_file_synIds = set(df[tag])
+	return(unique_file_synIds)
+
+
+syn = connectToSynClient()
 # use-case: Multiple file views to annotate
 
 if len(fileViewIds) > 1:
-	for i, element in enumerate(fileViewIds):	
-		view = syn.tableQuery("select * from " + fileViewIds[i])
-		file_synIds = [f[2] for f in view]
-		file_synIds = set(file_synIds)  
+	for i, element in enumerate(fileViewIds):
+		index = i	
+		view_df = downloadQueryAndmakeDf(syn, query, index, fileViewIds)
+		file_synIds = uniqueIds(view_df, tag) 
 		fileView.append(file_synIds)
 		print(fileView)
 
 #  use-case: Only one file view to annotate 
 
 if len(fileViewIds) == 1:
-	view = syn.tableQuery("select * from " + fileViewIds[0])
-	view_df = view.asDataframe()
-	file_synIds = [f[2] for f in view]
-	file_synIds = set(file_synIds)
+	index = 0	
+	view_df = downloadQueryAndmakeDf(syn, query, index, fileViewIds)
+	file_synIds = uniqueIds(view_df, tag) 
 	fileView.append(file_synIds)
 	print(fileView[0])
 
-# subset annotation dictionary based on available annotations 
-
-files_to_annot = set(t_data.keys()) & fileView[0]
-print("Number of files with annotations: " + str(len(files_to_annot)))
-print(files_to_annot)
-
-files_missing_annot = set(t_data.keys()) - fileView[0]
-print("Number of files missing annotations: \n" + str(len(files_missing_annot)))
-
-# which don't have annotation (place null/none annotations for them in file view)
-# files_missing_annot = [fileView[0] for fileView[0] in files_to_annot if files_to_annot.isdisjoint(fileView[0])]
-# print("files missing annotations: \n", files_missing_annot)
-
-''' 
-	for each row/ syn file ID
-	syn.get() File entity 
-	update File entity annotation 
-
-'''
-for i in files_to_annot:
-	synEntity = syn.get(i, downloadFile = False)
-	annotDicts = t_data.get(i)
-	synEntity.annotations.update(annotDicts)	# will overwrite the current annotations 
-	synEntity = syn.store(synEntity)
-	#print(synEntity) 							# test 
-
-
-
-
-
-
-
+print(view_df)
