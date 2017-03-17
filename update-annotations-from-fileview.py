69d830adaa51f380535deec27d934b94a5657327
teslajoy <nasim@plenary.org>
2017-03-17 16:06:12 -0700

    file-viewannotations update to save changes in a csv file

--- update-annotations-from-fileview.py
+++ update-annotations-from-fileview.py
@@ -1,27 +1,28 @@
-# Get a generator file view make changes to data frame push up changes to file view 
-# !!! Only works if file view (and its associated schema) exist !!! 
-
+'''Get file View query and based on user-defined schema change the anootation values of each matching field 
+   !!! Only works if file view (and its associated schema) exist !!! 
+'''
 import synapseclient
 import synapseutils 
 import pandas 
+import os 
 
-path = 'userDefined_annotations.csv'
-
-# list of existing file views and associated existing schema to be annotated 
+src_path = 'userDefined_annotations.csv'
 fileViewIds = ["syn8455906"] 
 fileView = []
 query = "select * from "
-tag = "id"
+tag = "id" # is this consistant through synapse 
+file_name = 'updated_annotations.csv'
+
+# TODO: check src and dst absolute path 
+# TODO: check user input 
 
 def getCSV(path):
-	# use'synapseId'as col index for now 
-	# TODO: pass args to panda read csv
+	path = os.path.abspath(path)
 	df = pandas.read_csv(path, index_col='synapseId')
 	return(df)
 
 def fastAccess(df):
-	# column or key access is faster 
-	df = df.transpose() 
+	df = df.T 
 	dc = df.to_dict()
 	return df, dc
 
@@ -39,25 +40,39 @@ def uniqueIds(df, tag):
 	unique_file_synIds = set(df[tag])
 	return(unique_file_synIds)
 
+if '.csv' not in src_path: 
+	print("User-defined annotations is not a csv file")
+	
+else:
+	syn = connectToSynClient()
 
-syn = connectToSynClient()
-# use-case: Multiple file views to annotate
+	'''use-case: Multiple file-views to annotate'''
+	if len(fileViewIds) > 1:
+		for i, element in enumerate(fileViewIds):
+			index = i	
+			view_df = downloadQueryAndmakeDf(syn, query, index, fileViewIds)
+			file_synIds = uniqueIds(view_df, tag) 
+			fileView.append(file_synIds)
+			# print(fileView)
 
-if len(fileViewIds) > 1:
-	for i, element in enumerate(fileViewIds):
-		index = i	
+	'''use-case: Only one file-view to annotate''' 
+	if len(fileViewIds) == 1:
+		index = 0	
 		view_df = downloadQueryAndmakeDf(syn, query, index, fileViewIds)
 		file_synIds = uniqueIds(view_df, tag) 
 		fileView.append(file_synIds)
-		print(fileView)
+		# print(fileView[0])
 
-#  use-case: Only one file view to annotate 
+	user_df = getCSV(src_path)
 
-if len(fileViewIds) == 1:
-	index = 0	
-	view_df = downloadQueryAndmakeDf(syn, query, index, fileViewIds)
-	file_synIds = uniqueIds(view_df, tag) 
-	fileView.append(file_synIds)
-	print(fileView[0])
+	'''make files indecies of your dataframe'''
+	if tag in view_df.columns: 
+		view_df = view_df.set_index(tag)
+		view_df.index.name = 'synapseId'
 
-print(view_df)
+	'''replace annotation based on user annotations where synID's match (pandas matches indexes)'''
+	view_df[user_df.columns] = user_df
+
+	'''save as csv file'''
+	dst_path = os.path.abspath('.') + file_name
+	view_df.to_csv(path_or_buf=dst_path, sep=',')
