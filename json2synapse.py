9a959812db5659a3288b9fcca639d7b628216d82
teslajoy <nasim@plenary.org>
2017-05-15 17:38:37 -0700

    concats a list of df from json to one df

--- json2synapse.py
+++ json2synapse.py
@@ -1,12 +1,12 @@
 import json
 import urllib
 import pandas
+import numpy
 import argparse
 import flatten_json
 import synapseclient 
 from flatten_json import flatten
 from pandas.io.json import json_normalize
-
 
 syn = synapseclient.login()
 
@@ -19,11 +19,33 @@ nf_path = 'https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotations
 ngs_path = 'https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotations/master/synapseAnnotations/data/ngs_annotations.json'
 onc_path = 'https://raw.githubusercontent.com/Sage-Bionetworks/synapseAnnotations/master/synapseAnnotations/data/onc_annotations.json'
 
+paths = [standard_path, analysis_path, dhart_path, genie_path, neuro_path, nf_path, ngs_path, onc_path]
+names = ['standard', 'analysis', 'dhart', 'genie', 'neuro', 'nf', 'ngs', 'onc']
+annotations = []
 
-raw_dat = urllib.urlopen(standard_path).read()
-json_record  = json.loads(raw_dat)
-record_flatten = (flatten(d) for d in json_record)
-df = pandas.DataFrame(record_flatten)
+def json2flatten(path, project):	
+	# fetch and read raw json objects from its github url and decode the json object to its raw format  
+	raw_dat = urllib.urlopen(standard_path).read()
+	json_record  = json.loads(raw_dat)
+	# Apply flatten (https://github.com/amirziai/flatten) to each element of json record and read it into a pandas dataframe
+	record_flatten = (flatten(d) for d in json_record)
+	df = pandas.DataFrame(record_flatten)
+	# Replace NaN objects with empty strings 
+	df = df.replace(numpy.nan, '', regex=True)
+	df.loc[:,'category'] = project
+	return(df)
+json2flatten(nf_path, 'nf')
+
+for project, path in [(n, url) for n in names for url in paths]:
+	data = json2flatten(path, project)
+	annotations.append(data)
+	# print(data)
+
+annotations_df = pandas.concat(annotations)
+print(annotations_df, annotations_df.shape)
+
+
+
 
 
 
